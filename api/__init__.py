b37477732fa5f0a0534745f9af34dca2e11d32a3
boo <booblegum42@gmail.com>
2022-03-02 04:12:37 -0800

    fix requirements and config

--- api/__init__.py
+++ api/__init__.py
@@ -5,16 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-class UnicodeApi(Api):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.app.config['RESTFUL_JSON'] = {
-            'ensure_ascii': False,
-        }
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
-api = UnicodeApi(app)
+api = Api(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-import os
+from pathlib import Path
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).parent
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'test.db')
+    JSON_AS_ASCII = False
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{BASE_DIR / 'main.db'}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     PORT = 5000
