c54bcdace41e4ad265ea207b2587c8e689ca0eda
boo <booblegum42@gmail.com>
2022-03-28 10:59:32 -0700

    Double Authors Res

--- api/resources/author.py
+++ api/resources/author.py
@@ -1,30 +1,15 @@
 from api import Resource, reqparse, db
 from api.models.author import AuthorModel
 
-# GET: /authors - authors list
+
 # GET: /authors/1 - author by id
 class AuthorResource(Resource):
-    def get(self, author_id=None):  # Если запрос приходит по url: /authors
-        if author_id is None:
-            authors = AuthorModel.query.all()
-            authors_list = [author.to_dict() for author in authors]
-            return authors_list, 200
-
-        # Если запрос приходит по url: /authors/<int:author_id>
+    def get(self, author_id):
         author = AuthorModel.query.get(author_id)
-        if author:
+        if author is None:
             return f"Author id={author_id} not found", 404
 
         return author.to_dict(), 200
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("name", required=True)
-        author_data = parser.parse_args()
-        author = AuthorModel(author_data["name"])
-        db.session.add(author)
-        db.session.commit()
-        return author.to_dict(), 201
 
     def put(self, author_id):
         parser = reqparse.RequestParser()
@@ -39,3 +24,20 @@ class AuthorResource(Resource):
 
     def delete(self, quote_id):
         raise NotImplemented("Метод не реализован")
+
+
+# GET: /authors - authors list
+class AuthorsListResource(Resource):
+    def get(self):
+        authors = AuthorModel.query.all()
+        authors_list = [author.to_dict() for author in authors]
+        return authors_list, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True)
+        author_data = parser.parse_args()
+        author = AuthorModel(author_data["name"])
+        db.session.add(author)
+        db.session.commit()
+        return author.to_dict(), 201
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from api import api, app
 from api.resources.quote import QuoteResource
-from api.resources.author import AuthorResource
+from api.resources.author import AuthorResource, AuthorsListResource
 from config import Config
 
 api.add_resource(QuoteResource,
@@ -9,7 +9,8 @@ api.add_resource(QuoteResource,
                  '/quotes'
                  )  # <-- requests
 api.add_resource(AuthorResource,
-                 '/authors/<int:author_id>',
+                 '/authors/<int:author_id>')  # <-- requests
+api.add_resource(AuthorsListResource,
                  '/authors')  # <-- requests
 
 if __name__ == '__main__':
