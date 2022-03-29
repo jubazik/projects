1b9dd7fbdbd8703c575da356e1276f18b6a49936
boo <booblegum42@gmail.com>
2022-03-29 11:29:50 -0700

    new User

--- api/models/author.py
+++ api/models/author.py
@@ -4,10 +4,12 @@ from api import db
 class AuthorModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32))
+    surname = db.Column(db.String(64), nullable=False, server_default="Default")
     quotes = db.relationship('QuoteModel', backref='author', lazy='dynamic')
 
-    def __init__(self, name):
+    def __init__(self, name, surname):
         self.name = name
+        self.surname = surname
 
     def to_dict(self):
         return {
---
+++ api/models/user.py
@@ -0,0 +1,18 @@
+from api import db
+from passlib.apps import custom_app_context as pwd_context
+
+
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, password):
+        self.username = username
+        self.hash_password(password)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
