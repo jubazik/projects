73db6f1ea9e2bdb3c65c781d27aab18bee1d0a59
boo <booblegum42@gmail.com>
2022-04-01 10:47:02 -0700

    add Tag

--- api/models/note.py
+++ api/models/note.py
@@ -1,12 +1,21 @@
 from api import db
 from api.models.user import UserModel
 
+from api.models.tag import TagModel
 
-class NoteModel(db.Model):
+# CORE
+tags = db.Table('tags_to_notes',
+                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
+                db.Column('note_model_id', db.Integer, db.ForeignKey('note_model.id'), primary_key=True)
+                )
+
+
+class NoteModel(db.Model):  # ORM
     id = db.Column(db.Integer, primary_key=True)
     author_id = db.Column(db.Integer, db.ForeignKey(UserModel.id))
     text = db.Column(db.String(255), unique=False, nullable=False)
     private = db.Column(db.Boolean(), default=True, nullable=False)
+    tags = db.relationship(TagModel, secondary=tags, lazy='subquery', backref=db.backref('notes', lazy=True))
 
     def save(self):
         db.session.add(self)
