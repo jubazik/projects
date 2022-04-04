a1628c6033b3969671fdaa515f2290792cc09948
boo <booblegum42@gmail.com>
2022-04-04 10:43:10 -0700

    add fixture load models

--- load_fixture.py
+++ load_fixture.py
@@ -1,4 +1,4 @@
-import os
+import json
 from api import db
 from api.schemas.user import UserRequestSchema
 from config import BASE_DIR, base_dir
@@ -8,18 +8,18 @@ from api.models.note import NoteModel
 from api.models.user import UserModel
 
 # path_to_fixture = os.path.join(base_dir, "fixtures", "users.json")
-path_to_fixture = BASE_DIR / "fixtures" / "users.json"
+path_to_fixture = BASE_DIR / "fixtures" / "notes.json"
+models = {
+    "NoteModel": NoteModel,
+    "UserModel": UserModel,
+}
 with open(path_to_fixture, "r", encoding="UTF-8") as f:
-    users_data = UserRequestSchema(many=True).loads(f.read())
+    data = json.load(f)
     users_created = 0
-    for user_data in users_data:
-        user = UserModel(**user_data)
-        db.session.add(user)
-        try:
-            db.session.commit()
-            users_created += 1
-        except IntegrityError:
-            db.session.rollback()
-            print(f"User {user.username} already exists")
+    model_name = data["model"]
+    model = models[model_name]
+    for record in data["records"]:
+        model_object = model(**record)
+        model_object.save()
 
-    print(f"{users_created} users created")
+    print(f"??? users created")
