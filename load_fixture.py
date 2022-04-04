e5da6aa875b8664106f06e0d3d7fca64da808bf4
boo <booblegum42@gmail.com>
2022-04-04 11:29:05 -0700

    load fixtures

--- load_fixture.py
+++ load_fixture.py
@@ -1,3 +1,4 @@
+import click
 import json
 from api import db
 from api.schemas.user import UserRequestSchema
@@ -7,19 +8,23 @@ from sqlalchemy.exc import IntegrityError
 from api.models.note import NoteModel
 from api.models.user import UserModel
 
-# path_to_fixture = os.path.join(base_dir, "fixtures", "users.json")
-path_to_fixture = BASE_DIR / "fixtures" / "notes.json"
-models = {
-    "NoteModel": NoteModel,
-    "UserModel": UserModel,
-}
-with open(path_to_fixture, "r", encoding="UTF-8") as f:
-    data = json.load(f)
-    users_created = 0
-    model_name = data["model"]
-    model = models[model_name]
-    for record in data["records"]:
-        model_object = model(**record)
-        model_object.save()
 
-    print(f"??? users created")
+@click.command
+@click.option('--fixture', help='fixture name .json')
+def load_fixture(fixture):
+    path_to_fixture = BASE_DIR / "fixtures" / fixture
+    models = {
+        "NoteModel": NoteModel,
+        "UserModel": UserModel,
+    }
+    with open(path_to_fixture, "r", encoding="UTF-8") as f:
+        data = json.load(f)
+        model_name = data["model"]
+        model = models[model_name]
+        for record in data["records"]:
+            model_object = model(**record)
+            model_object.save()
+
+
+if __name__ == "__main__":
+    load_fixture()
