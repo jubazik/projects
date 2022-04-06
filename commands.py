a42fd33ca3b7c93e7c4ad3ac912427a35096051e
boo <booblegum42@gmail.com>
2022-04-06 10:37:22 -0700

    add new commands

--- commands.py
+++ commands.py
@@ -1,4 +1,5 @@
-from api import app
+import click
+from api import app, db
 from api.models.user import UserModel
 
 
@@ -7,8 +8,39 @@ def create_superuser():
     """
     Creates a user with the admin role
     """
-    username = input("Username[default 'admin']:")
-    password = input("Password[default 'admin']:")
+    username = input("Username[default 'admin']:") or "admin"
+    password = input("Password[default 'admin']:") or "admin"
     user = UserModel(username, password, role="admin", is_staff=True)
     user.save()
-    print(f"Superuser create successful! id={user.id}")
+    if user.id:
+        print(f"Superuser create successful! id={user.id}")
+    else:
+        print(f"User with {user.username} already exists")
+
+
+@app.cli.command('user')
+@click.argument('do')  # create delete show
+@click.option("--all", is_flag=True, default=False)
+def user_operation(do, all):
+    """
+    User operations
+    :create: create/show-all/delete
+    """
+    if do == "delete":
+        if all:
+            UserModel.query.delete()
+            db.session.commit()
+            print("Delete all users")
+            return
+        username = input("username: ")
+        user = UserModel.query.filter_by(username=username).first()
+        if user:
+            user.delete()
+            print("User deleted")
+        else:
+            print(f"User with {username} not found")
+
+    elif do == "show-all":
+        users = UserModel.query.all()
+        for user in users:
+            print(f"{user.username}")
